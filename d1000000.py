1f8a500c009485536a4dcd7c4849f409521f572d
Naman Modani <modani.naman@gmail.com>
2022-04-02 16:19:45 -0700

    Problem 3: d1000000

---
+++ d1000000.py
@@ -0,0 +1,31 @@
+# Google CodeJam 2022
+# d1000000
+# Naman Modani
+
+from collections import defaultdict
+INF = 1e15
+
+def solve():
+  N = int(input())
+  dice = [int(x) for x in input().split(' ')]
+  dice.sort()
+
+  count = 0
+  for die in dice:
+    if die > count:
+      count += 1
+
+  return count
+
+def main():
+
+  N = int(input())
+  results = []
+
+  for _ in range(N):
+    results.append(solve())
+
+  for i in range(len(results)):
+    print(f'Case #{i+1}: {results[i]}')
+
+main()
