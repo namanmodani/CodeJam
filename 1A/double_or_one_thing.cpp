66cda3ce4416bc283053e6ce88e8be5bc0a58f64
Naman Modani <modani.naman@gmail.com>
2022-04-08 19:47:20 -0700

    1A Problem 1 Solved (?)

---
+++ 1A/double_or_one_thing.cpp
@@ -0,0 +1,57 @@
+// Google Code Jam 2022
+// Double or One Thing
+// Naman Modani
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+  
+  int t;
+  cin >> t;
+
+  for (int r = 0; r < t; ++r) {
+    string s;
+    cin >> s;
+    string v;
+    v.push_back(s[0]);
+    int i = 0;
+    
+    while (i + 1 < s.size()) {
+      char cur = v.back();
+      char next = s[i + 1];
+      int diff = (int)next - (int)cur;
+      
+      if (diff > 0) {
+        v.push_back(cur);
+        v.push_back(next);
+      } 
+      
+      else if (diff < 0) {
+        v.push_back(next);
+      } 
+      
+      else {
+        string h;
+        h.push_back(cur);
+        h += s.substr(i + 1);
+        string g = s.substr(i + 1);
+        
+        if (h.compare(g) <= 0) {
+          v.push_back(cur);
+          v.push_back(next);
+        } 
+        
+        else {
+          v.push_back(next);
+        }
+      }
+      ++i;
+    }
+    printf("Case #%d: %s\n", r + 1, v.c_str());
+  }
+  return 0;
+}
---
+++ 1A/equal_sum.py
@@ -0,0 +1,4 @@
+# Google Code Jam 2022
+# Equal Sum
+# Naman Modani
+
